41dad5ec0cdccc14cacdfc5ced017f9e7f61631b
Vasco Bartolomeu <vascobartolomeu@MacBook-Air-de-Vasco-2.local>
2022-11-03 11:27:52 +0000

    Trial for basic queries

--- script.py
+++ script.py
@@ -2,6 +2,16 @@ from mando import command, main
 
 @command
 def race_script():
+    conn = psycopg2.connect(
+    host="localhost",
+    port="5433",
+    database="newdb",
+    user="postgres",
+    password="1234",
+    options='-c search_path=public')
+
+
+    cur = conn.cursor()
     restart = True
     
     while restart:
@@ -15,22 +25,23 @@ def race_script():
         
         restart = False
         menu_option = input("Choose an option by typing a number: ")
+        
         if menu_option == "1":
             restart = False
             menu_1 = input("Please type the name of the runner you are searching for: ")
-            if menu_1 == "Vasco":
-                print("He didnt run he is fat")
-                print("")
-                restart = True
+            
+            cursor.execute("SELECT name FROM runner WHERE runner.name==(%s)", (menu_1))
+            restart = True
 
         elif menu_option == "2":
             restart = False
             menu_1 = input("Please type the name of the race you are searching for: ")
-            print("")
-            menu_0 = "0"
+            
+            cursor.execute("SELECT name FROM race WHERE race.name==(%s)", (menu_1))
+            restart = True
         else:
             return
-    
+        
     
 if __name__ == '__main__':
     main()
