5431bd0e5a83f3e5b5b776fb35db56acd6bc48e5
RojanAsl <116028259+RojanAsl@users.noreply.github.com>
2022-11-05 18:12:16 +0000

    Add files via upload

--- races.sql
+++ races.sql
@@ -1,18 +1,11 @@
 CREATE TABLE runner (
     id INTEGER PRIMARY KEY,                    -- runner id
-    name VARCHAR(64) NOT NULL,                 -- name
-    birth_date DATE NOT NULL,                  -- birthday FORMAT: 'MM-DD-YYYY'
+    name VARCHAR(64) NOT NULL,                 -- name  
 	sex VARCHAR (1) CHECK (sex IN ('M', 'F')), -- gender
     nation VARCHAR (2),                        -- nationality
+    birth_date DATE NOT NULL,                  -- birthday FORMAT: 'MM-DD-YYYY'
     UNIQUE (name, birth_date)                  -- person with the same name on the same birthday is not allowed
 );
-
--- UNIQUENESS OF team, age_class, event names should be verified in backend (python)
--- UNLESS: 
---CREATE TABLE test(
---    id SERIAL UNIQUE,   -- age_class id
---    name VARCHAR PRIMARY KEY -- age_class  
---);
 
 CREATE TABLE team (
     id INTEGER PRIMARY KEY,   -- team id
@@ -30,24 +23,34 @@ CREATE TABLE event (
 );
 
 CREATE TABLE race (
-    id INTEGER PRIMARY KEY,                     -- race id
-	id_event INTEGER NOT NULL REFERENCES event, -- event id
-    distance INTEGER NOT NULL,                  -- distance (km)
-	year INTEGER NOT NULL                       -- race year
+    id INTEGER PRIMARY KEY,                                       -- race id
+	id_event INTEGER NOT NULL REFERENCES event ON DELETE CASCADE, -- event id
+	year INTEGER NOT NULL,                                        -- race year
+    distance INTEGER NOT NULL                                     -- distance (km)
+
 );
 
+-----
+CREATE TABLE race (
+    id INTEGER PRIMARY KEY,                                       -- race id
+	id_event INTEGER NOT NULL REFERENCES event ON DELETE CASCADE, -- event id
+	year INTEGER NOT NULL,                                        -- race year
+    distance INTEGER NOT NULL                                     -- distance (km)
+);
+-----
 CREATE TABLE participation (
-    id_runner INTEGER REFERENCES runner,                -- This runner is enrolled
-    id_race INTEGER REFERENCES race,                    -- in this race
-	bib INTEGER,                                        -- bib
-    id_age_class INTEGER REFERENCES age_class NOT NULL, -- in this age_class
-    id_team INTEGER REFERENCES team,                    -- team if of the runner (if any)
-	place_overall INTEGER,                              -- place over all age classes
-	place_in_class INTEGER,                             -- place in their age_class
-	official_time TIME,                                 -- official time to finish the race
-    net_time TIME,                                      -- net time to finish the race
-    PRIMARY KEY (id_runner, id_race),                   -- runners cannot enrole twice in the same race
-    UNIQUE (id_race, bib)                               -- bib is unique within every race
+    id INTEGER,
+    id_team INTEGER REFERENCES team ON DELETE CASCADE,                    -- team if of the runner (if any)
+	bib INTEGER,                                                          -- bib
+    id_runner INTEGER REFERENCES runner ON DELETE CASCADE,                -- This runner is enrolled
+    id_race INTEGER REFERENCES race ON DELETE CASCADE,                    -- in this race
+    id_age_class INTEGER REFERENCES age_class NOT NULL ON DELETE CASCADE, -- in this age_class
+	place_in_class INTEGER,                                               -- place in their age_class
+    net_time TIME,                                                        -- net time to finish the race
+	official_time TIME,                                                   -- official time to finish the race
+    place_overall INTEGER,                                                -- place over all age classes
+    PRIMARY KEY (id_runner, id_race),                                     -- runners cannot enrole twice in the same race
+    UNIQUE (id_race, bib)                                                 -- bib is unique within every race
 );
 
 
@@ -66,7 +69,8 @@ INSERT INTO age_class VALUES (2, 'M20');
 INSERT INTO event VALUES (1, 'dia do pai');
 INSERT INTO event VALUES (2, 'xmass');
 
-INSERT INTO race VALUES (1, 1, 10, 2015 );
-INSERT INTO race VALUES (2, 2, 15, 2015 );
+INSERT INTO race VALUES (1, 1, 15, 2015 );
+INSERT INTO race VALUES (3, 1, 15, 2016 );
+INSERT INTO race VALUES (2, 1, 15, 2017 );
 
 INSERT INTO participation VALUES (1, 1, 1, 1, 1, 2, 1, '01:17:43', '01:17:43');
